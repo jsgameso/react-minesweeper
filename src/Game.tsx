dfd94599c8666cd43d5536a3f5827127d2388b59
Eduardo Grajales Villanueva <edgravill@gmail.com>
2019-09-03 09:57:59 -0500

    Last features and style

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
-import Minesweeper from '@jsgames/minesweeper';
+import Minesweeper, { GameStatus, Board } from '@jsgames/minesweeper';
 
 const Game: React.FC = () => {
-  const [board, setBoard] = React.useState<(number | null)[][]>(Array(10).fill(Array(10).fill(null)));
-  const [startedGame, setStartedGame] = React.useState<boolean>(false);
   const [game] = React.useState<Minesweeper>(new Minesweeper());
+  const [gameStatus, setGameStatus] = React.useState<GameStatus>(game.status);
+  const [board, setBoard] = React.useState<Board>(game.board);
+
+  React.useEffect(() => {
+    game.on('board', setBoard);
+    game.on('game', setGameStatus);
+  }, [game]);
 
   const reveal = (x: number, y: number) => () => {
-    if (!startedGame) {
-      setBoard(game.newGame([x, y]));
-      setStartedGame(true);
+    if (gameStatus === 'active') {
+      game.reveal([x, y]);
     } else {
-      const nextBoard = game.reveal([x, y]);
-      console.log(nextBoard)
-      setBoard(nextBoard);
+      game.newGame([x, y]);
     }
   }
 
@@ -22,21 +24,36 @@ const Game: React.FC = () => {
       <style>
         {`
           button {
-            height: 2rem;
-            width: 2rem;
+            align-items: center;
+            background-color: transparent;
+            border: 2px solid gray;
+            cursor: pointer;
+            display: flex;
+            font-family: sans-serif;
+            font-size: 1rem;
+            font-weight: bold;
+            height: 2.5rem;
+            justify-content: center;
+            width: 2.5rem;
+          }
+
+          button:focus {
+            outline: none;
           }
         `}
       </style>
       <table>
-        {board.map((row, x) => (
-          <tr key={x}>
-            {row.map((cell, y) => (
-              <td key={`${x}-${y}`}>
-                <button onClick={reveal(x, y)}>{cell === null ? '' : cell === 10 ? 'X' : cell}</button>
-              </td>
-            ))}
-          </tr>
-        ))}
+        <tbody>
+          {board.map((row, y) => (
+            <tr key={y}>
+              {row.map((cell, x) => (
+                <td key={`${x}-${y}`}>
+                  <button onClick={reveal(x, y)}>{cell === null ? '' : cell === 10 ? '💣' : cell}</button>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );
