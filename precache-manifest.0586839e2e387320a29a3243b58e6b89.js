5b48dbf9aaf9fcefb838cb5dc191653f7130c5ec
Eduardo Grajales Villanueva <edgravill@gmail.com>
2019-09-05 22:23:02 -0500

    Path updated

---
+++ precache-manifest.0586839e2e387320a29a3243b58e6b89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de9c588f8e4084295a9bbf0afea9d31",
+    "url": "/react-minesweeper/index.html"
+  },
+  {
+    "revision": "1b1cf59ecbcad354b464",
+    "url": "/react-minesweeper/static/js/2.d931c64b.chunk.js"
+  },
+  {
+    "revision": "29ce03c9dce5fc93186d",
+    "url": "/react-minesweeper/static/js/main.63b32026.chunk.js"
+  },
+  {
+    "revision": "09b7bd90b2b0fd733840",
+    "url": "/react-minesweeper/static/js/runtime~main.7ad09fa7.js"
+  },
+  {
+    "revision": "8264bc9522a687aa22fcb28cdc6199d7",
+    "url": "/react-minesweeper/static/media/bomb.8264bc95.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f2111a8d21b13839976b009397891b5.js"
+  "/react-minesweeper/precache-manifest.0586839e2e387320a29a3243b58e6b89.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-minesweeper/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
