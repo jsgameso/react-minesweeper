9ce9668efd1fae8ca7a2fe02773e22565d49e746
Eduardo Grajales Villanueva <edgravill@gmail.com>
2019-09-05 22:13:31 -0500

    Pages

---
+++ precache-manifest.8f2111a8d21b13839976b009397891b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4a7bf89ddf12e04a4d0990f06bb974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2387a9e26d0c6ac83785",
+    "url": "/static/js/2.22a811f3.chunk.js"
+  },
+  {
+    "revision": "933da674421b607a41d1",
+    "url": "/static/js/main.6fa40bfe.chunk.js"
+  },
+  {
+    "revision": "5f90bc66bc58f9c8d7fd",
+    "url": "/static/js/runtime~main.c0d74dce.js"
+  },
+  {
+    "revision": "6e1de49fc28745959204113b9274a94e",
+    "url": "/static/media/bomb.6e1de49f.svg"
+  }
+]);
